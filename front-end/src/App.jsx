f5a3b0984eddb29f4ccc6f568feca12e060ee511
mbahmu25 <nikogenta25@gmail.com>
2024-06-07 17:25:41 +0700

    add socket io client to fornt-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState,useEffect } from 'react'
-
+import { io } from "socket.io-client";
 import './App.css'
 import { MapContainer, TileLayer, Circle} from "react-leaflet";
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [dataGPS, setGPS] = useState("");
+  useEffect(() => {
+    const socket = io("http://localhost:3000/", {
+      transports: ["websocket"],
+    });
+    socket.on("connect", () => {
+      console.log("Nyambung ngab");
+    });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+    socket.on("message", (newData) => {
+      console.log(newData);
+      setGPS(newData);
+    });
+    return () => socket.emit("end");
+  }, [dataGPS]);
   return (
     <>
       {/* <div className='bg-red-500'>Tes</div> */}
@@ -13,6 +28,12 @@ function App() {
           attribution=''
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Circle
+          center={dataGPS.length > 0 ?([parseFloat(dataGPS[1]), parseFloat(dataGPS[2])]):([0,0])}
+          radius={10}
+          fillColor="red"
+          color="red"
+          className="bg-color-red"/>
       </MapContainer>
     </>
   )
